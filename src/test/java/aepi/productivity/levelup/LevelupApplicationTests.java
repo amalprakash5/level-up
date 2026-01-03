1837d19f15af332bfa3d68c16515f55a8ae843ad
AMAL PRAKASH <76277810+amalprakash5@users.noreply.github.com>
2026-01-03 13:26:48 +0530

    Inital Commit

---
+++ src/test/java/aepi/productivity/levelup/LevelupApplicationTests.java
@@ -0,0 +1,13 @@
+package aepi.productivity.levelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
